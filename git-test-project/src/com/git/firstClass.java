5accb617300eb12a739ccf31882858db3f9d72e1
quanli <quanli>
2019-03-17 11:04:18 +0800

    项目添加到本地仓库

---
+++ git-test-project/src/com/git/firstClass.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+    }
+    public void test(){
+        int i=0;
+        i+=10;
+    }
+}
